8d25c3cc6c622a8721ba8adad4970e33a1038c95
vaishnavij224 <vaishnavijagdale7654@gmail.com>
2024-05-16 19:50:45 +0530

    add() added

--- file1.java
+++ file1.java
@@ -13,13 +13,5 @@ public class File1
 	 System.out.println("First Java");
 	 System.out.println("My Second Line of Code"); 		
 	}
-=======
-  public static void main(String[] args) {
-    System.out.println("java code");
-    System.out.println("hiiii welcome to java");
-    //hiii
-    add();
-  }
->>>>>>> 25b8d7555e258e6dec464e4c62766c011c76daea
 }
 
