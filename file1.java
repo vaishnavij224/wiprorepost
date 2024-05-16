bc93e0c17e9ee67bf303a243ca025eb45f9e0661
vaishnavij224 <vaishnavijagdale7654@gmail.com>
2024-05-16 19:48:22 +0530

    add() added

--- file1.java
+++ file1.java
@@ -1,5 +1,6 @@
 import java.lang.*;
 
+
 public class File1
 {
 	int x=1000;
@@ -12,5 +13,13 @@ public class File1
 	 System.out.println("First Java");
 	 System.out.println("My Second Line of Code"); 		
 	}
+=======
+  public static void main(String[] args) {
+    System.out.println("java code");
+    System.out.println("hiiii welcome to java");
+    //hiii
+    add();
+  }
+>>>>>>> 25b8d7555e258e6dec464e4c62766c011c76daea
 }
 
