afa88d6e04ac4da9f0abaa6e29adfb350fdb5f94
VaishnavI Ajit Jagadale <vaishnavijagdale7654@gmail.com>
2024-05-16 18:27:21 +0530

    Update file1.java

--- file1.java
+++ file1.java
@@ -2,6 +2,7 @@ public class file1 {
 
   public static void main(String[] args) {
     System.out.println("java code");
-    System.out.println("hiiii welcome to java")
+    System.out.println("hiiii welcome to java");
+    //hiii
   }
 }
